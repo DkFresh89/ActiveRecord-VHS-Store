636bd937ec6950340ff114e584b9db13421a2efc
Doug <doug.kerzner@gmail.com>
2021-02-15 16:23:28 -0500

    Complete

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,4 +4,26 @@ class Genre < ActiveRecord::Base
     has_many :movies, through: :movie_genres
 
 
+    def self.most_popular 
+
+        Genre.all.sort_by{|genre| genre.movies.count}.reverse[0..4]
+
+    end 
+
+    def average_movie_length
+
+        sum = self.movies.sum{|movies| movies.length}
+        return 0 if sum == 0
+        #sets to a float and rounds to 2 decimal points
+        average = sum/self.movies.count.to_f.round(2)
+
+    end 
+
+    def self.longest_movies
+
+        self.all.max_by{|genre| genre.average_movie_length}
+
+    end 
+
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
     has_many :vhs 
     has_many :rentals, through: :vhs 
 
-
+    #Easy!!!
     def self.longest
         # binding.pry 
         self.all.sort_by{|movie| movie.length}.reverse
@@ -31,9 +31,59 @@ class Movie < ActiveRecord::Base
     #Using instance method number_of_clients on an instance of a movie.
     #Max_by enumerable to iterate over all movie instances and apply the block on each instance
     #Returns the most clients of that movie instance
-    def self.most_clients
-        self.all.max_by{|movie| movie.number_of_clients}
-    end
+    # def self.most_clients
+    #     self.all.max_by{|movie| movie.number_of_clients}
+    # end
+
+    def movie_clients
+        self.rentals.map(&:client)
+    end 
+
+
+    def self.most_clients 
+        movies_hash = self.all.each_with_object({}) {|movie, movie_hash| movie_hash[movie] = movie.movie_clients.uniq.count}
+        movies_hash.max_by{|movie, client_count| client_count} [0]
+    end 
+
+    def self.most_rentals
+
+        self.all.sort_by{|movie| movie.number_of_rentals}.reverse[0..2]
+
+    end 
+
+    def self.most_popular_female_director
+
+        female_director_movies = self.all.select{|movie| movie.female_director}
+
+        female_director_movies.max_by{|movie| movie.number_of_rentals}.director 
+        #.director retruns the string of the the director attached to the instance
+
+    end 
+    #Easy method
+    def self.newest_list
+        Movie.all.sort_by{|movie| movie.year}.reverse 
+    end 
+
+    def recommendation
+
+        emoji = ["🥬", "🔥", "⭐️"].sample
+        puts emoji + " title: #{self.title} \n description: #{self.description} \n length: #{self.length} \n director: #{self.director} \n year: #{self.year}"
+
+    end 
+    #Easy
+    def self.surprise_me
+
+        self.all.sample.recommendation
+
+    end 
+
+    def report_stolen
+
+        Vhs.available_now.select{|vhs| vhs.movie == self}.sample.destroy
+        puts "THANK YOU FOR YOUR REPORT. WE WILL LAUNCH AN INVESTIGATION."
+
+    end 
+
 
 
 end
